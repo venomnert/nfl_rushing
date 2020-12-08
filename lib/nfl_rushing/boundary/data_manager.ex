02e76e835b748afbf9f0e07e46ffd26a7470ff24
venomnert <neerthigan@gmail.com>
2020-12-08 15:49:17 -0500

    Updated genserver to utilize core api

--- lib/nfl_rushing/boundary/data_manager.ex
+++ lib/nfl_rushing/boundary/data_manager.ex
@@ -1,13 +1,17 @@
 defmodule NflRushing.Boundary.DataManager do
+  @data_file Application.app_dir(:nfl_rushing, "priv/static/rushing_small.json")
+
   use GenServer
 
-  @data_file Application.app_dir(:nfl_rushing, "priv/static/rushing_small.json")
   alias NflRushing.Core.Rushing
-
 
   #  Client
   def get() do
     GenServer.call(__MODULE__, :get)
+  end
+
+  def reset_filter() do
+    GenServer.call(__MODULE__, :reset_filter)
   end
 
   def sort(sort_msg) do
@@ -29,7 +33,7 @@ defmodule NflRushing.Boundary.DataManager do
 
   @impl true
   def init(_) do
-    init_state = %{data: %{}, filtered_data: %{}}
+    init_state = %{data: [], filtered_data: [], headers: []}
 
     {:ok, init_state, {:continue, :get_data}}
   end
@@ -38,13 +42,21 @@ defmodule NflRushing.Boundary.DataManager do
   def handle_continue(:get_data, state) do
     data = @data_file |> Rushing.load_file()
     updated_state = Map.put(state, :data, data)
+                    |> Map.put(:headers, Rushing.get_headers(data))
+                    |> Map.put(:filtered_data, data)
 
     {:noreply, updated_state}
   end
 
   @impl true
   def handle_call(:get, _from, state) do
-    {:reply, state.data, state}
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_call(:reset_filter, _from, state) do
+    updated_state = Map.put(state, :filtered_data, state.data)
+    {:reply, updated_state, updated_state}
   end
 
   @impl true
@@ -58,7 +70,6 @@ defmodule NflRushing.Boundary.DataManager do
   @impl true
   def handle_call({:sort, sort_msg}, _from, state) do
     filtered_data = state.filtered_data |> Rushing.sort(sort_msg)
-    # filtered_data = state.data |> Rushing.sort(sort_msg)
     updated_state = Map.put(state, :filtered_data, filtered_data)
 
     {:reply, filtered_data, updated_state}
@@ -66,8 +77,8 @@ defmodule NflRushing.Boundary.DataManager do
 
   @impl true
   def handle_call(:export, _from, state) do
-    Process.sleep(4000)
-    {:reply, @data_file, state}
-  end
+    url = state.filtered_data |> Rushing.create_csv(state.headers)
 
+    {:reply, url, state}
+  end
 end
