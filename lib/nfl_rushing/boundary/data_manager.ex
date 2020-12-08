0e499d2a547d3016407129c857ccf0db537211e9
venomnert <neerthigan@gmail.com>
2020-12-08 17:30:46 -0500

    Utilize load_file_stream for reading data

--- lib/nfl_rushing/boundary/data_manager.ex
+++ lib/nfl_rushing/boundary/data_manager.ex
@@ -1,13 +1,18 @@
 defmodule NflRushing.Boundary.DataManager do
-  @data_file Application.app_dir(:nfl_rushing, "priv/static/rushing_small.json")
+  @data_file Application.app_dir(:nfl_rushing, "priv/static/rushing.json")
 
   use GenServer
 
   alias NflRushing.Core.Rushing
+  @chunks 5
 
   #  Client
   def get() do
     GenServer.call(__MODULE__, :get)
+  end
+
+  def next() do
+    GenServer.call(__MODULE__, :next)
   end
 
   def reset_filter() do
@@ -33,17 +38,18 @@ defmodule NflRushing.Boundary.DataManager do
 
   @impl true
   def init(_) do
-    init_state = %{data: [], filtered_data: [], headers: []}
+    init_state = %{data: [], filtered_data: [], headers: [], next_stream: nil}
 
     {:ok, init_state, {:continue, :get_data}}
   end
 
   @impl true
   def handle_continue(:get_data, state) do
-    data = @data_file |> Rushing.load_file()
+    {data, next_stream} = @data_file |> Rushing.load_file_stream(@chunks)
     updated_state = Map.put(state, :data, data)
                     |> Map.put(:headers, Rushing.get_headers(data))
                     |> Map.put(:filtered_data, data)
+                    |> Map.put(:next_stream, next_stream)
 
     {:noreply, updated_state}
   end
@@ -56,6 +62,21 @@ defmodule NflRushing.Boundary.DataManager do
   @impl true
   def handle_call(:reset_filter, _from, state) do
     updated_state = Map.put(state, :filtered_data, state.data)
+
+    IO.inspect(length(state.data), label: "MAIN DATA")
+    IO.inspect(length(updated_state.filtered_data), label: "FILTER DATA")
+
+    {:reply, updated_state, updated_state}
+  end
+
+  @impl true
+  def handle_call(:next, _from, state) do
+    {data, next_stream} = state.next_stream |> Rushing.next(@chunks)
+    updated_data = state.data ++ data
+    updated_state = Map.put(state, :data, updated_data)
+                    |> Map.put(:filtered_data, updated_data)
+                    |> Map.put(:next_stream, next_stream)
+
     {:reply, updated_state, updated_state}
   end
 
